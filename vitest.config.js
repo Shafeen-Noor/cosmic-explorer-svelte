79288f015ee2b35c84947230321eb515f22f9148
Shafeen Noor <72748751+Shafeen-Noor@users.noreply.github.com>
2026-04-03 08:41:05 +0500

    Final project

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [
@@ -14,6 +15,9 @@ export default defineConfig({
 		setupFiles: ['./tests/setup.js']
 	},
 	resolve: {
+		alias: {
+			$lib: path.resolve('./src/lib') // <- add this line
+		},
 		conditions: ['browser']
 	}
 });
